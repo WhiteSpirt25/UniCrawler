7e2f742d206299181d8d49f52acc62b60f974c2c
Alexandr Spiridonov <alexandr.spy@gmail.com>
2021-10-13 13:51:20 +0300

    Limited crawler to main domain. Subdomains blocked by regex using 1-st allowed domain. Added counter for unique subdomains and urls for subdomain sites. Renamed all custom counters with 'my_' in front for easier readability of stats.

--- crawler/uni_parsing/spiders/scrapy_crawler.py
+++ crawler/uni_parsing/spiders/scrapy_crawler.py
@@ -8,6 +8,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import re
+
 # Removed PDF, DOC and DOCX so they could be parsed
 MY_IGNORED_EXTENSIONS = [
     # archives
@@ -42,8 +44,14 @@ class ExampleSpider(CrawlSpider):
     start_urls = [os.getenv('START_URL')] # Start with this one
     #start_urls = ["https://dspace.spbu.ru/handle/11701/21736"] # File test url
 
+    subdomains = set()
+
     rules = [
-        Rule(LinkExtractor(), callback='page_download', follow=True),
+        # Rule for pages in subdomain to count their amount
+        Rule(LinkExtractor(allow = '\.' + allowed_domains[0]), callback='subdomain_counter', follow=False),
+        # Rule for pages that are NOT in subdomain to download
+        # Blocks only 1-st allowed domain
+        Rule(LinkExtractor(deny='\.' + allowed_domains[0]), callback='page_download', follow=True),
         # Rule for files, regexpr searches for urls with pdf,doc or docx on the end.
         Rule(LinkExtractor(allow = '.*\.pdf',deny_extensions=MY_IGNORED_EXTENSIONS), callback ='pdf_download'),
         Rule(LinkExtractor(allow = '.*\.doc',deny_extensions=MY_IGNORED_EXTENSIONS), callback ='doc_download'),
@@ -59,21 +67,30 @@ class ExampleSpider(CrawlSpider):
             }
         reqs.post(url=API_URL,data=data,files=files)
 
+    def subdomain_counter(self, response):
+        print('Subdomain %s.' % response.url)
+        self.crawler.stats.inc_value('my_subdomain_urls')
+        subdomain = re.findall(r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)',response.url)[0]
+        if subdomain not in self.subdomains:
+            self.crawler.stats.inc_value('my_subdomain_count')
+            self.subdomains.add(subdomain)
+
+
     def page_download(self, response):
-        #print('Got a response from %s.' % response.url)
+        print('Main domain %s.' % response.url)
         self._post_handler(response.url,response.body)
 
     def pdf_download(self, response):
         #print('Got a response from %s.' % response.url)
-        self.crawler.stats.inc_value('pdf_files_met')
+        self.crawler.stats.inc_value('my_pdf_files_met')
         self._post_handler(response.url,response.body)
     
     def doc_download(self, response):
         #print('Got a response from %s.' % response.url)
-        self.crawler.stats.inc_value('doc_files_met')
+        self.crawler.stats.inc_value('my_doc_files_met')
         self._post_handler(response.url,response.body)
 
     def docx_download(self, response):
         #print('Got a response from %s.' % response.url)
-        self.crawler.stats.inc_value('docx_files_met')
+        self.crawler.stats.inc_value('my_docx_files_met')
         self._post_handler(response.url,response.body)
