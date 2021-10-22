f004258cdb094f58d9211b1bc1ce63066e1a95f4
Edem Topuzov <keltikan@gmail.com>
2021-10-23 00:07:34 +0300

    Update tests

--- crawler/tests/crawler_test.py
+++ crawler/tests/crawler_test.py
@@ -15,14 +15,18 @@ with description('Crawler') as self:
 
     with it('counts links quantity correctly w/o duplicates'):
         expect(self.stats.get('scheduler/enqueued', 0) +
-            self.stats.get('scheduler/dequeued', 0) +
-            self.stats.get('offsite/filtered', 0)
-        ).to(equal(2052))
+            self.stats.get('offsite/filtered', 0) -
+            self.stats.get('retry/count', 0)
+        ).to(equal(1029))
 
     with it('counts inner pages correctly'):
-        expect(self.stats.get('scheduler/enqueued', 0) +
-            self.stats.get('scheduler/dequeued', 0)
-        ).to(equal(2048))
+        expect(self.stats.get('my_pages_downloaded', 0)).to(equal(1006))
+
+    with it('counts inner subdomain urls correctly'):
+        expect(self.stats.get('my_subdomain_urls', 0)).to(equal(3))
+
+    with it('counts inner subdomains correctly'):
+        expect(self.stats.get('my_subdomain_count', 0)).to(equal(2))
 
     with it('counts links to redirected pages correctly'):
         expect(
@@ -31,16 +35,18 @@ with description('Crawler') as self:
 
     with it('counts not working links correctly'):
         expect(
-            sum({k: v for k, v in self.stats.items() if re.match("downloader/response_status_count/[45]", k)}.values())
-        ).to(equal(4))
+            sum({k: v for k, v in self.stats.items() if re.match("downloader/response_status_count/[45]", k)}.values()) -
+            sum({k: v for k, v in self.stats.items() if re.match("retry/reason_count/[45]", k)}.values())
+        ).to(equal(2))
 
     with it('counts links to absent pages correctly'):
         expect(self.stats.get('downloader/response_status_count/404', 0)).to(equal(1))
 
     with it('counts links to server errors pages correctly'):
         expect(
-            sum({k: v for k, v in self.stats.items() if re.match("downloader/response_status_count/5", k)}.values())
-        ).to(equal(3))
+            sum({k: v for k, v in self.stats.items() if re.match("downloader/response_status_count/5", k)}.values()) -
+            sum({k: v for k, v in self.stats.items() if re.match("retry/reason_count/5", k)}.values())
+        ).to(equal(1))
 
     with it('counts offsite pages correctly'):
         expect(self.stats.get('offsite/filtered', 0)).to(equal(4))
